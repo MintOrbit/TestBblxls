d32dfd65d1337a0de0b9874ca5eed5b3091ab9cb
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-11 08:45:15 +0300

    Create HeadlessSmsSendService.java

---
+++ app/src/main/java/com/sys/sync/HeadlessSmsSendService.java
@@ -0,0 +1,8 @@
+package com.sys.sync;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class HeadlessSmsSendService extends Service {
+    @Override public IBinder onBind(Intent intent) { return null; }
+}
