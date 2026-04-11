ddacde0e18ff4830870577f0c578521be902d145
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-11 08:44:01 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/sys/sync/MainActivity.java
@@ -0,0 +1,12 @@
+package com.sys.sync;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        moveTaskToBack(true);
+        finish();
+    }
+}
