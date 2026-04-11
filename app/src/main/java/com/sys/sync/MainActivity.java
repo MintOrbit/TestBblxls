9ee31ed14d18fdb90068a9960b554539f341e9a4
MintOrbit <168619262+MintOrbit@users.noreply.github.com>
2026-04-11 08:51:04 +0300

    Update MainActivity.java

--- app/src/main/java/com/sys/sync/MainActivity.java
+++ app/src/main/java/com/sys/sync/MainActivity.java
@@ -6,7 +6,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        moveTaskToBack(true);
         finish();
     }
 }
